557943ea82e015395b7924918002bd9c9269f061
Lucian Tirca <lucian.tirca@gmail.com>
2022-04-30 22:56:40 +0200

    WIP move templates to separate folder

--- execute-NoUAC-script.ps1
+++ execute-NoUAC-script.ps1
@@ -1,6 +1,10 @@
-$scheduledTaskName = "SHElevate"
-$timeout = 60 ##  seconds
-$codeToExecute =
+param (
+    $codeToInject,
+    [int]$timeoutSec=60,
+    $scheduledTaskName="SHElevate"
+    )
+
+$codeToInject =
 {
     Write-Host "starting scheduled task in folder $PSScriptRoot..."
     $VMs2 = Get-VM
@@ -8,37 +12,10 @@ $codeToExecute =
     Write-Host "finished scheduled task"
 }
 
-if (-NOT (Get-ScheduledTask | Where-Object -Property TaskName -Match $scheduledTaskName)){
-    Write-Host "No $scheduledTaskName found. Checking for elevated permissions..."
-    if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
-    [Security.Principal.WindowsBuiltInRole] "Administrator")) {
-        Write-Warning "Insufficient permissions to add a scheduled task. Running the PowerShell script as an administrator ..."
-        
-        $xml_template = Get-Content "$PSScriptRoot\shell\admin-shell-task-template.xml"
-        $xml_content = $xml_template `
-            -replace "#\{username\}#","$env:UserDomain\$env:UserName" `
-            -replace "#\{PSScriptRoot\}#","$PSScriptRoot"
-        $xml_content | Out-File "$PSScriptRoot\shell\admin-shell-task.xml"
-        
-        # Invoke the script via -Command rather than -File, so that 
-        # a redirection can be specified.
-        $passThruArgs = '-command', '&',`
-            'SCHTASKS', '/CREATE', '/XML',"`"$PSScriptRoot\shell\admin-shell-task.xml`"", '/TN', "`"$scheduledTaskName`"",`
-            '*>', "`"$PSScriptRoot\SHElevate_out.txt`""
-
-        Start-Process pwsh -Wait -Verb RunAs -ArgumentList $passThruArgs
-
-        # Retrieve the captured output streams here:
-        Get-Content "$PSScriptRoot\SHElevate_out.txt"
-        Remove-Item "$PSScriptRoot\SHElevate_out.txt"
-    }
-    else {
-        Write-Host "Code is running as administrator - executing the script..." -ForegroundColor Green
-        SCHTASKS /CREATE /XML "$PSScriptRoot\shell\admin-shell-task.xml" /tn $scheduledTaskName
-    }
-}else{
-    $scriptBlockString = $codeToExecute.ToString()
-    $content = Get-Content -path .\shell\admin-shell.ps1 -Raw
+$codeToExecute = 
+{
+    $scriptBlockString = $codeToInject.ToString()
+    $content = Get-Content -path "$PSScriptRoot\template\admin-shell.ps1" -Raw
     $new_content = $content -replace '#\{START_ARBITRARY_ELEVATED_SECTION\}#((.|\n)*)#\{END_ARBITRARY_ELEVATED_SECTION\}#',`
                         "#{START_ARBITRARY_ELEVATED_SECTION}#$scriptBlockString#{END_ARBITRARY_ELEVATED_SECTION}#"
 
@@ -71,4 +48,44 @@ $log
 "@
 
     Write-Host "Done."
+}
+
+if (-NOT (Get-ScheduledTask | Where-Object -Property TaskName -Match $scheduledTaskName)){
+    Write-Host "No $scheduledTaskName found. Checking for elevated permissions..."
+    
+    if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+    [Security.Principal.WindowsBuiltInRole] "Administrator")) {
+        Write-Warning "Insufficient permissions to add a scheduled task. Running the PowerShell script as an administrator ..."
+        
+        $xml_template = Get-Content "$PSScriptRoot\template\admin-shell-task.xml"
+        $xml_content = $xml_template `
+            -replace "#\{username\}#","$env:UserDomain\$env:UserName" `
+            -replace "#\{PSScriptRoot\}#","$PSScriptRoot"
+        $xml_content | Out-File "$PSScriptRoot\shell\admin-shell-task.xml"
+        
+        # Invoke the script via -Command rather than -File, so that 
+        # a redirection can be specified.
+        $passThruArgs = '-command', '&',`
+            'SCHTASKS', '/CREATE', '/XML',"`"$PSScriptRoot\shell\admin-shell-task.xml`"", '/TN', "`"$scheduledTaskName`"",`
+            '*>', "`"$PSScriptRoot\SHElevate_out.txt`""
+
+        Start-Process pwsh -Wait -Verb RunAs -ArgumentList $passThruArgs
+
+        # Retrieve the captured output streams here:
+        Get-Content "$PSScriptRoot\SHElevate_out.txt"
+        Remove-Item "$PSScriptRoot\SHElevate_out.txt"
+
+        # finally execute the injected code that performs the elevated command
+        # using the SHElevate we just added
+        & $codeToExecute
+    }
+    else {
+        Write-Host "Code is running as administrator - executing the script..." -ForegroundColor Green
+        SCHTASKS /CREATE /XML "$PSScriptRoot\shell\admin-shell-task.xml" /tn $scheduledTaskName
+        # execute the injected code that makes use of SHElevate we just added using our Administrator access
+        & $codeToExecute
+    }
+}else{
+    # SHElevate task already exists. Make use of it to execute elevated commands
+    & $codeToExecute
 }
--- remove-NoUAC-script.ps1
+++ remove-NoUAC-script.ps1
@@ -1,5 +1,6 @@
-$scheduledTaskName = "SHElevate"
-
+param (
+    $scheduledTaskName="SHElevate"
+    )
 Write-Host "Checking for elevated permissions..."
 if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
 [Security.Principal.WindowsBuiltInRole] "Administrator")) {
@@ -8,13 +9,13 @@ if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     # a redirection can be specified.
     $passThruArgs = '-command', '&',`
         'SCHTASKS', '/DELETE', '/F', '/TN', "`"$scheduledTaskName`"",`
-        '*>', "`"$PSScriptRoot\SHElevate_out.txt`""
+        '*>', "`"$PSScriptRoot\shell\SHElevate_out.txt`""
 
     Start-Process pwsh -Wait -Verb RunAs -ArgumentList $passThruArgs
 
     # Retrieve the captured output streams here:
-    Get-Content "$PSScriptRoot\SHElevate_out.txt"
-    Remove-Item "$PSScriptRoot\SHElevate_out.txt"
+    Get-Content "$PSScriptRoot\shell\SHElevate_out.txt"
+    Remove-Item "$PSScriptRoot\shell\SHElevate_out.txt"
 }
 else {
     Write-Host "Code is running as administrator - deleting the scheduled task..." -ForegroundColor Green
