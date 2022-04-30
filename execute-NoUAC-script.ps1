637329920ed8118603ff9b940fa75180bcdca900
Lucian Tirca <lucian.tirca@gmail.com>
2022-04-30 22:04:23 +0200

    WIP add shell scheduled task conditionally and run e2e

--- execute-NoUAC-script.ps1
+++ execute-NoUAC-script.ps1
@@ -1,45 +1,74 @@
-
-$scheduledTaskName = "NoUAC exec scriptBlock"
+$scheduledTaskName = "SHElevate"
 $timeout = 60 ##  seconds
 $codeToExecute =
 {
     Write-Host "starting scheduled task in folder $PSScriptRoot..."
-    $VMs = Get-VM
-    $VMs |  Export-Csv "$PSScriptRoot\VMs.csv"
+    $VMs2 = Get-VM
+    $VMs2 |  Export-Csv "$PSScriptRoot\VMs.csv"
     Write-Host "finished scheduled task"
 }
 
-$scriptBlockString = $codeToExecute.ToString()
-$content = Get-Content -path .\shell\admin-shell.ps1 -Raw
-$new_content = $content -replace '#\{START_ARBITRARY_ELEVATED_SECTION\}#((.|\n)*)#\{END_ARBITRARY_ELEVATED_SECTION\}#',`
-                    "#{START_ARBITRARY_ELEVATED_SECTION}#$scriptBlockString#{END_ARBITRARY_ELEVATED_SECTION}#"
+if (-NOT (Get-ScheduledTask | Where-Object -Property TaskName -Match $scheduledTaskName)){
+    Write-Host "No $scheduledTaskName found. Checking for elevated permissions..."
+    if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+    [Security.Principal.WindowsBuiltInRole] "Administrator")) {
+        Write-Warning "Insufficient permissions to add a scheduled task. Running the PowerShell script as an administrator ..."
+        
+        $xml_template = Get-Content "$PSScriptRoot\shell\admin-shell-task-template.xml"
+        $xml_content = $xml_template `
+            -replace "#\{username\}#","$env:UserDomain\$env:UserName" `
+            -replace "#\{PSScriptRoot\}#","$PSScriptRoot"
+        $xml_content | Out-File "$PSScriptRoot\shell\admin-shell-task.xml"
+        
+        # Invoke the script via -Command rather than -File, so that 
+        # a redirection can be specified.
+        $passThruArgs = '-command', '&',`
+            'SCHTASKS', '/CREATE', '/XML',"`"$PSScriptRoot\shell\admin-shell-task.xml`"", '/TN', "`"$scheduledTaskName`"",`
+            '*>', "`"$PSScriptRoot\SHElevate_out.txt`""
 
-$new_content | Out-File "$PSScriptRoot\shell\admin-shell.ps1" -Force -NoNewline
-$task = Get-ScheduledTask | Where-Object -Property TaskName -Match $scheduledTaskName
-$task | Start-ScheduledTask          
+        Start-Process pwsh -Wait -Verb RunAs -ArgumentList $passThruArgs
 
-$timer =  [Diagnostics.Stopwatch]::StartNew()
+        # Retrieve the captured output streams here:
+        Get-Content "$PSScriptRoot\SHElevate_out.txt"
+        Remove-Item "$PSScriptRoot\SHElevate_out.txt"
+    }
+    else {
+        Write-Host "Code is running as administrator - executing the script..." -ForegroundColor Green
+        SCHTASKS /CREATE /XML "$PSScriptRoot\shell\admin-shell-task.xml" /tn $scheduledTaskName
+    }
+}else{
+    $scriptBlockString = $codeToExecute.ToString()
+    $content = Get-Content -path .\shell\admin-shell.ps1 -Raw
+    $new_content = $content -replace '#\{START_ARBITRARY_ELEVATED_SECTION\}#((.|\n)*)#\{END_ARBITRARY_ELEVATED_SECTION\}#',`
+                        "#{START_ARBITRARY_ELEVATED_SECTION}#$scriptBlockString#{END_ARBITRARY_ELEVATED_SECTION}#"
 
-while (((Get-ScheduledTask -TaskName $scheduledTaskName).State -ne  'Ready') -and  ($timer.Elapsed.TotalSeconds -lt $timeout)) {    
+    $new_content | Out-File "$PSScriptRoot\shell\admin-shell.ps1" -Force -NoNewline
+    $task = Get-ScheduledTask | Where-Object -Property TaskName -Match $scheduledTaskName
+    $task | Start-ScheduledTask          
 
-Write-Verbose  -Message "Waiting on scheduled task..."
+    $timer =  [Diagnostics.Stopwatch]::StartNew()
 
-Start-Sleep -Seconds 1
+    while (((Get-ScheduledTask -TaskName $scheduledTaskName).State -ne  'Ready') -and  ($timer.Elapsed.TotalSeconds -lt $timeout)) {    
 
-}
+    Write-Verbose  -Message "Waiting on scheduled task..."
 
-$timer.Stop()
-Write-Verbose  -Message "We waited [$($timer.Elapsed.TotalSeconds)] seconds on the task '$scheduledTaskName'."
+    Start-Sleep -Seconds 1
 
-if ($timer.Elapsed.TotalSeconds -ge $timeout){
-    Write-Host "Timed out after $timeout seconds, forcefully terminating ..." 
-    $task | Stop-ScheduledTask
-}
+    }
 
-$log = Get-Content "$PSScriptRoot\shell\admin-shell-output.log" -Raw
-Write-Host @"
+    $timer.Stop()
+    Write-Verbose  -Message "We waited [$($timer.Elapsed.TotalSeconds)] seconds on the task '$scheduledTaskName'."
+
+    if ($timer.Elapsed.TotalSeconds -ge $timeout){
+        Write-Host "Timed out after $timeout seconds, forcefully terminating ..." 
+        $task | Stop-ScheduledTask
+    }
+
+    $log = Get-Content "$PSScriptRoot\shell\admin-shell-output.log" -Raw
+    Write-Host @"
 ===========Log from task===========
 $log
 "@
 
-Write-Host "Done."
+    Write-Host "Done."
+}
