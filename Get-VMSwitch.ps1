04927a8c7195213215b3f5797d9f3730c21f51e4
Lucian Tirca <lucian.tirca@gmail.com>
2022-05-01 08:50:14 +0200

    WIP Get-VMSwitch

---
+++ Get-VMSwitch.ps1
@@ -0,0 +1,12 @@
+
+$GetVMSwitch_codeToInject = {
+    $VMSwitches = Get-VMSwitch
+    $VMSwitches |  Export-Csv "$PSScriptRoot\VMSwitches.csv" -Verbose
+    $VMSwitches
+}
+
+function PSHyperVLabNet\Get-VMSwitch {
+    & $PSScriptRoot\execute-NoUAC-shell.ps1 -timeout 2 -codeStringToInject $GetVMSwitch_codeToInject.ToString()
+    $VMSwitches = Import-Csv -Path "$PSScriptRoot\shell\VMSwitches.csv"
+    $VMSwitches
+}
