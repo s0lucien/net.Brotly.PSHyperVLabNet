60f876a84d1ecc9169d8bc5e0db5b73264f9b162
Lucian Tirca <lucian.tirca@gmail.com>
2022-05-01 14:28:28 +0200

    WIP PSSerializer for Get-VMSwitch

--- Get-VMSwitch.ps1
+++ Get-VMSwitch.ps1
@@ -1,13 +1,15 @@
 
 $GetVMSwitch_codeToInject = {
     $VMSwitches = Get-VMSwitch -Verbose
-    $VMSwitches |  Export-Csv "$PSScriptRoot\VMSwitches.csv" -Verbose
+    $ser = [System.Management.Automation.PSSerializer]::Serialize($VMSwitches)
+    $ser | Out-File "$PSScriptRoot\VMSwitches.PSSerialized"
     $VMSwitches
 }
 
 function PSHyperVLabNet\Get-VMSwitch {
     & $PSScriptRoot\execute-NoUAC-shell.ps1 -codeStringToInject $GetVMSwitch_codeToInject.ToString()
-    $VMSwitches = Import-Csv -Path "$PSScriptRoot\shell\VMSwitches.csv"
+    $VMSwitches_out = Get-Content "$PSScriptRoot\shell\VMSwitches.PSSerialized"
+    $VMSwitches =[System.Management.Automation.PSSerializer]::Deserialize($VMSwitches_out)
     $VMSwitches
 }
 
