bba8e7dc43ae543e7641e9978f8d0188669cd6e2
Lucian Tirca <lucian.tirca@gmail.com>
2022-05-01 14:24:07 +0200

    WIP PSSerializer for Get-VM

--- Get-VM.ps1
+++ Get-VM.ps1
@@ -1,13 +1,15 @@
 
 $GetVM_codeToInject = {
     $VMs = Get-VM -Verbose
-    $VMs |  Export-Csv "$PSScriptRoot\VMs.csv" -Verbose
+    $ser = [System.Management.Automation.PSSerializer]::Serialize($VMs)
+    $ser | Out-File "$PSScriptRoot\VMs.PSSerialized"
     $VMs
 }
 
 function PSHyperVLabNet\Get-VM {
     & $PSScriptRoot\execute-NoUAC-shell.ps1 -timeout 2 -codeStringToInject $GetVM_codeToInject.ToString()
-    $VMs = Import-Csv -Path "$PSScriptRoot\shell\VMs.csv"
+    $VMs_out = Get-Content "$PSScriptRoot\shell\VMs.PSSerialized"
+    $VMs =[System.Management.Automation.PSSerializer]::Deserialize($VMs_out)
     $VMs
 }
 
