888be3a1880e468c09593aa39ca8c418ed4b24c8
Lucian Tirca <lucian.tirca@gmail.com>
2022-05-02 09:14:59 +0200

    WIP setup-vNet example

--- example/setup-vNet.ps1
+++ example/setup-vNet.ps1
@@ -1,52 +1,95 @@
-function Setup-HyperVLabNet {
+function Setup-HyperVLabNet ($NetPrefix = "BrotlyNet") {
     ### Create vNICs
     # raspberry MACs
-    PSHyperVLabNet\Add-VMNetworkAdapter -VMName "rpi" -SwitchName "BrotlyNet_host"
-    PSHyperVLabNet\Add-VMNetworkAdapter -VMName "rpi" -SwitchName "BrotlyNet_pf2vm"
+
+    #IDEMPOTENT : remove before add to start from clean state
+    PSHyperVLabNet\Remove-VMNetworkAdapter -VMName "rpi" -SwitchName "$($NetPrefix)_host"
+    PSHyperVLabNet\Remove-VMNetworkAdapter -VMName "rpi" -SwitchName "$($NetPrefix)_pf2vm"
+    #END IDEMPOTENT
+    PSHyperVLabNet\Add-VMNetworkAdapter -VMName "rpi" -SwitchName "$($NetPrefix)_host"
+    PSHyperVLabNet\Add-VMNetworkAdapter -VMName "rpi" -SwitchName "$($NetPrefix)_pf2vm"
     
     # winfra MACs
-    PSHyperVLabNet\Add-VMNetworkAdapter -VMName "WinSrv2022" -SwitchName "BrotlyNet_host"
-    PSHyperVLabNet\Add-VMNetworkAdapter -VMName "WinSrv2022" -SwitchName "BrotlyNet_pf2vm"
+    #IDEMPOTENT : remove before add to start from clean state
+    PSHyperVLabNet\Remove-VMNetworkAdapter -VMName "WinSrv2022" -SwitchName "$($NetPrefix)_host"
+    PSHyperVLabNet\Remove-VMNetworkAdapter -VMName "WinSrv2022" -SwitchName "$($NetPrefix)_pf2vm"
+    #END IDEMPOTENT
+    PSHyperVLabNet\Add-VMNetworkAdapter -VMName "WinSrv2022" -SwitchName "$($NetPrefix)_host"
+    PSHyperVLabNet\Add-VMNetworkAdapter -VMName "WinSrv2022" -SwitchName "$($NetPrefix)_pf2vm"
 
     # linfra MACs
-    PSHyperVLabNet\Add-VMNetworkAdapter -VMName "Ubuntu22.04" -SwitchName "BrotlyNet_host"
-    PSHyperVLabNet\Add-VMNetworkAdapter -VMName "Ubuntu22.04" -SwitchName "BrotlyNet_pf2vm"
+    #IDEMPOTENT : remove before add to start from clean state
+    PSHyperVLabNet\Remove-VMNetworkAdapter -VMName "Ubuntu22.04" -SwitchName "$($NetPrefix)_host"
+    PSHyperVLabNet\Remove-VMNetworkAdapter -VMName "Ubuntu22.04" -SwitchName "$($NetPrefix)_pf2vm"
+    #END IDEMPOTENT
+    PSHyperVLabNet\Add-VMNetworkAdapter -VMName "Ubuntu22.04" -SwitchName "$($NetPrefix)_host"
+    PSHyperVLabNet\Add-VMNetworkAdapter -VMName "Ubuntu22.04" -SwitchName "$($NetPrefix)_pf2vm"
 
     # hv MACs
-    PSHyperVLabNet\Add-VMNetworkAdapter -VMName "HyperVSrv2019" -SwitchName "BrotlyNet_host"
-    PSHyperVLabNet\Add-VMNetworkAdapter -VMName "HyperVSrv2019" -SwitchName "BrotlyNet_pf2vm"
+    #IDEMPOTENT : remove before add to start from clean state
+    PSHyperVLabNet\Remove-VMNetworkAdapter -VMName "HyperVSrv2019" -SwitchName "$($NetPrefix)_host"
+    PSHyperVLabNet\Remove-VMNetworkAdapter -VMName "HyperVSrv2019" -SwitchName "$($NetPrefix)_pf2vm"
+    #END IDEMPOTENT
+    PSHyperVLabNet\Add-VMNetworkAdapter -VMName "HyperVSrv2019" -SwitchName "$($NetPrefix)_host"
+    PSHyperVLabNet\Add-VMNetworkAdapter -VMName "HyperVSrv2019" -SwitchName "$($NetPrefix)_pf2vm"
 
 
     # # pfsense MACs
-    # PSHyperVLabNet\Add-VMNetworkAdapter -VMName "pfSense" -SwitchName "BrotlyNet_host"
-    # PSHyperVLabNet\Add-VMNetworkAdapter -VMName "pfSense" -SwitchName "BrotlyNet_pf2vm"
+    # #IDEMPOTENT : remove before add to start from clean state
+    # PSHyperVLabNet\Remove-VMNetworkAdapter -VMName "pfSense" -SwitchName "$($NetPrefix)_host"
+    # PSHyperVLabNet\Remove-VMNetworkAdapter -VMName "pfSense" -SwitchName "$($NetPrefix)_pf2vm"
+    # #END IDEMPOTENT
+    # PSHyperVLabNet\Add-VMNetworkAdapter -VMName "pfSense" -SwitchName "$($NetPrefix)_host"
+    # PSHyperVLabNet\Add-VMNetworkAdapter -VMName "pfSense" -SwitchName "$($NetPrefix)_pf2vm"
     # # share host internet connection using the NATed 'Default Switch'
     # PSHyperVLabNet\Add-VMNetworkAdapter -VMName "pfSense" -SwitchName "Default Switch"
 
     ### Create vSwitches
-    # PSHyperVLabNet\New-VMSwitch -name "BrotlyNet_pf2vm" -type "Private"
-    # PSHyperVLabNet\New-VMSwitch -name "BrotlyNet_host" -type "Internal"
+    # PSHyperVLabNet\New-VMSwitch -name "$($NetPrefix)_pf2vm" -type "Private"
+    # PSHyperVLabNet\New-VMSwitch -name "$($NetPrefix)_host" -type "Internal"
 
     ### Connect VMs to vSwitches
     # raspberry NICs
-    PSHyperVLabNet\Connect-VMNetworkAdapter -VMName "rpi" -SwitchName "BrotlyNet_host"
-    PSHyperVLabNet\Connect-VMNetworkAdapter -VMName "rpi" -SwitchName "BrotlyNet_pf2vm"
+    # IDEMPOTENT: remove existing connections to the NIC Mac addresses to start from a clean state
+    PSHyperVLabNet\Disconnect-VMNetworkAdapter -VMName "rpi" -SwitchName "$($NetPrefix)_host"
+    PSHyperVLabNet\Disconnect-VMNetworkAdapter -VMName "rpi" -SwitchName "$($NetPrefix)_pf2vm"
+    # END IDEMPOTENT
+    PSHyperVLabNet\Disconnect-VMNetworkAdapter -VMName "rpi" -SwitchName "$($NetPrefix)_host"
+    PSHyperVLabNet\Disconnect-VMNetworkAdapter -VMName "rpi" -SwitchName "$($NetPrefix)_pf2vm"
 
     # winfra NICs
-    PSHyperVLabNet\Connect-VMNetworkAdapter -VMName "WinSrv2022" -SwitchName "BrotlyNet_host"
-    PSHyperVLabNet\Connect-VMNetworkAdapter -VMName "WinSrv2022" -SwitchName "BrotlyNet_pf2vm"
+    # IDEMPOTENT: remove existing connections to the NIC Mac addresses to start from a clean state
+    PSHyperVLabNet\Disconnect-VMNetworkAdapter -VMName "WinSrv2022" -SwitchName "$($NetPrefix)_host"
+    PSHyperVLabNet\Disconnect-VMNetworkAdapter -VMName "WinSrv2022" -SwitchName "$($NetPrefix)_pf2vm"
+    # END IDEMPOTENT    
+    PSHyperVLabNet\Connect-VMNetworkAdapter -VMName "WinSrv2022" -SwitchName "$($NetPrefix)_host"
+    PSHyperVLabNet\Connect-VMNetworkAdapter -VMName "WinSrv2022" -SwitchName "$($NetPrefix)_pf2vm"
 
-    # linfra NICss
-    PSHyperVLabNet\Connect-VMNetworkAdapter -VMName "Ubuntu22.04" -SwitchName "BrotlyNet_host"
-    PSHyperVLabNet\Connect-VMNetworkAdapter -VMName "Ubuntu22.04" -SwitchName "BrotlyNet_pf2vm"
+    # linfra NICs
+    # IDEMPOTENT: remove existing connections to the NIC Mac addresses to start from a clean state
+    PSHyperVLabNet\Disconnect-VMNetworkAdapter -VMName "Ubuntu22.04" -SwitchName "$($NetPrefix)_host"
+    PSHyperVLabNet\Disconnect-VMNetworkAdapter -VMName "Ubuntu22.04" -SwitchName "$($NetPrefix)_pf2vm"
+    # END IDEMPOTENT    
+    PSHyperVLabNet\Connect-VMNetworkAdapter -VMName "Ubuntu22.04" -SwitchName "$($NetPrefix)_host"
+    PSHyperVLabNet\Connect-VMNetworkAdapter -VMName "Ubuntu22.04" -SwitchName "$($NetPrefix)_pf2vm"
 
-    # hv NICss
-    PSHyperVLabNet\Connect-VMNetworkAdapter -VMName "HyperVSrv2019" -SwitchName "BrotlyNet_host"
-    PSHyperVLabNet\Connect-VMNetworkAdapter -VMName "HyperVSrv2019" -SwitchName "BrotlyNet_pf2vm"
+    # hv NICs
+    # IDEMPOTENT: remove existing connections to the NIC Mac addresses to start from a clean state
+    PSHyperVLabNet\Disconnect-VMNetworkAdapter -VMName "HyperVSrv2019" -SwitchName "$($NetPrefix)_host"
+    PSHyperVLabNet\Disconnect-VMNetworkAdapter -VMName "HyperVSrv2019" -SwitchName "$($NetPrefix)_pf2vm"
+    # END IDEMPOTENT
+    PSHyperVLabNet\Connect-VMNetworkAdapter -VMName "HyperVSrv2019" -SwitchName "$($NetPrefix)_host"
+    PSHyperVLabNet\Connect-VMNetworkAdapter -VMName "HyperVSrv2019" -SwitchName "$($NetPrefix)_pf2vm"
 
     # # pfsense NICs
-    # PSHyperVLabNet\Connect-VMNetworkAdapter -VMName "pfSense" -SwitchName "BrotlyNet_host"
-    # PSHyperVLabNet\Connect-VMNetworkAdapter -VMName "pfSense" -SwitchName "BrotlyNet_pf2vm"
+    # IDEMPOTENT: remove existing connections to the NIC Mac addresses to start from a clean state
+    PSHyperVLabNet\Disconnect-VMNetworkAdapter -VMName "pfSense" -SwitchName "$($NetPrefix)_host"
+    PSHyperVLabNet\Disconnect-VMNetworkAdapter -VMName "pfSense" -SwitchName "$($NetPrefix)_pf2vm"
+    # disconnect WAN vSwitch
+    PSHyperVLabNet\Disconnect-VMNetworkAdapter -VMName "pfSense" -SwitchName "Default Switch"
+    # END IDEMPOTENT
+    # PSHyperVLabNet\Connect-VMNetworkAdapter -VMName "pfSense" -SwitchName "$($NetPrefix)_host"
+    # PSHyperVLabNet\Connect-VMNetworkAdapter -VMName "pfSense" -SwitchName "$NetPrefix_pf2vm"
     # # connect to internet using the NATed 'Default Switch'
     # PSHyperVLabNet\Connect-VMNetworkAdapter -VMName "pfSense" -SwitchName "Default Switch"
 
