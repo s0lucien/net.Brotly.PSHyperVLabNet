ff26dbc4d73ae4d7087b22f180790ef7f0f2e09b
Lucian Tirca <lucian.tirca@gmail.com>
2022-05-03 16:53:14 +0200

    New-VMSwitch use -EncodedCommand shell

--- New-VMSwitch.ps1
+++ New-VMSwitch.ps1
@@ -1,20 +1,26 @@
+. .\Encode-Text-b64.ps1
 
 $NewVMSwitch_scriptBlockToInject = {
-    New-VMSwitch -Name $NewVMSwitch_Name -SwitchType $NewVMSwitch_SwitchType -Verbose
+    $SwitchName = $switch_name
+    $SwitchType = $switch_type
+   
+    New-VMSwitch -Name "$SwitchName`_$SwitchType" -SwitchType $SwitchType -Verbose
     $ser = [System.Management.Automation.PSSerializer]::Serialize($?)
-    $ser | Out-File "$PSScriptRoot\New-VMSwitch.PSSerialized"
+    $ser | Out-File "$(Get-Location)\New-VMSwitch.PSSerialized"
 }
 
-function PSHyperVLabNet\New-VMSwitch ($name, $type){
+#TODO: SWitchType is Internal|Private in CmdletBindings
+function PSHyperVLabNet\New-VMSwitch ($SwitchName, $SwitchType){
     $scriptBlockToInject = $NewVMSwitch_ScriptBlockToInject.ToString() `
-        -replace '\$NewVMSwitch_Name', "`"$name`"" `
-        -replace '\$NewVMSwitch_SwitchType', $type
-    & $PSScriptRoot\execute-NoUAC-shell.ps1 -codeStringToInject $scriptBlockToInject
+        -replace '\$switch_name', "`"$SwitchName`"" `
+        -replace '\$switch_type', "`"$SwitchType`""
+    $encodedCommand = Encode-Text-b64 -Text $scriptBlockToInject
+    & $PSScriptRoot\execute-NoUAC-shell.ps1 -codeStringToInject "pwsh -WorkingDirectory '$PSScriptRoot\shell\' -EncodedCommand $encodedCommand"
     $NewVMSwitch_out = Get-Content "$PSScriptRoot\shell\New-VMSwitch.PSSerialized"
     $SHElevate? =[System.Management.Automation.PSSerializer]::Deserialize($NewVMSwitch_out)
     $SHElevate?
 }
 
-# PSHyperVLabNet\New-VMSwitch -name "DEMO PRIVATE" -type "Private"
-# PSHyperVLabNet\New-VMSwitch -name "DEMO INTERNAL" -type "Internal"
+# PSHyperVLabNet\New-VMSwitch -SwitchName "DEMO PRIVATE" -SwitchType "Private"
+# PSHyperVLabNet\New-VMSwitch -SwitchName "DEMO INTERNAL" -SwitchType "Internal"
 
