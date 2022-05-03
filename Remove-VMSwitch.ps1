387f93d78554902c443e8374f4898c21474bb397
Lucian Tirca <lucian.tirca@gmail.com>
2022-05-03 17:18:48 +0200

    Remove-VMSwitch uses -EncodedCommand

--- Remove-VMSwitch.ps1
+++ Remove-VMSwitch.ps1
@@ -2,17 +2,17 @@
 $RemoveVMSwitch_scriptBlockToInject = {
     Remove-VMSwitch -Name $RemoveVMSwitch_Name -Force -Verbose
     $ser = [System.Management.Automation.PSSerializer]::Serialize($?)
-    $ser | Out-File "$PSScriptRoot\Remove-VMSwitch.PSSerialized"
+    $ser | Out-File "$(Get-Location)\Remove-VMSwitch.PSSerialized"
 }
 
 function PSHyperVLabNet\Remove-VMSwitch ($name){
     $scriptBlockToInject = $RemoveVMSwitch_ScriptBlockToInject.ToString() `
         -replace '\$RemoveVMSwitch_Name', "`"$name`""
-    & $PSScriptRoot\execute-NoUAC-shell.ps1 -codeStringToInject $scriptBlockToInject
+    $encodedCommand = Encode-Text-b64 -Text $scriptBlockToInject.ToString()
+    & $PSScriptRoot\execute-NoUAC-shell.ps1 -codeStringToInject "pwsh -WorkingDirectory '$PSScriptRoot\shell\' -EncodedCommand $encodedCommand"
     $RemoveVMSwitch_out = Get-Content "$PSScriptRoot\shell\Remove-VMSwitch.PSSerialized"
     $SHElevate? =[System.Management.Automation.PSSerializer]::Deserialize($RemoveVMSwitch_out)
     $SHElevate?
 }
 
-# PSHyperVLabNet\Remove-VMSwitch -name "DEMO PRIVATE"
-# PSHyperVLabNet\Remove-VMSwitch -name "DEMO INTERNAL"
+# PSHyperVLabNet\Remove-VMSwitch -name "TEST INTERNAL_Internal"
