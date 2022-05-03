4116681b4abf7b1236846460efb3bbc0ac3eb7a2
Lucian Tirca <lucian.tirca@gmail.com>
2022-05-03 09:12:10 +0200

    Refactor Get-VM.ps1 to use pwsh -EncodedCommand

--- Get-VM.ps1
+++ Get-VM.ps1
@@ -1,16 +1,21 @@
+. .\Encode-ScriptBlock-b64.ps1
 
 $GetVM_scriptBlockToInject = {
     $VMs = Get-VM -Verbose
     $ser = [System.Management.Automation.PSSerializer]::Serialize($VMs)
-    $ser | Out-File "$PSScriptRoot\VMs.PSSerialized"
+    Write-Host "Writing VMs to file $(Get-Location)\VMs.PSSerialized"
+    $ser | Out-File "$(Get-Location)\VMs.PSSerialized"
     $VMs
 }
 
 function PSHyperVLabNet\Get-VM {
-    & $PSScriptRoot\execute-NoUAC-shell.ps1 -codeStringToInject $GetVM_scriptBlockToInject.ToString()
+    $encodedCommand = Encode-ScriptBlock-b64 -ScriptBlockToEncode $GetVM_scriptBlockToInject
+    & $PSScriptRoot\execute-NoUAC-shell.ps1 -codeStringToInject "pwsh -WorkingDirectory '$PSScriptRoot\shell\' -EncodedCommand $encodedCommand"
+    Write-Host "Executed. Retrieving result"
     $VMs_out = Get-Content "$PSScriptRoot\shell\VMs.PSSerialized"
     $VMs =[System.Management.Automation.PSSerializer]::Deserialize($VMs_out)
     $VMs
 }
 
 # $VMs = PSHyperVLabNet\Get-VM
+# $VMs
